6291e1fce8b45e67e4f1fcdd270571ffa3e8efbd
RaffeLegend <oldgirafferiver@gmail.com>
2025-04-14 11:55:23 +0100

    [update] supervise all the opening

--- project/backend/app.py
+++ project/backend/app.py
@@ -40,4 +40,6 @@ def detect_image():
         return jsonify({'error': '服务器检测失败'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    # app.run(debug=True)
+    app.run(host='0.0.0.0', port=port)
