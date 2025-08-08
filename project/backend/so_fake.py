23ca9683948e196bb1e0abea0eead54edbaa4f00
RaffeLegend <oldgirafferiver@gmail.com>
2025-08-08 15:32:25 +0100

    try to fix

--- project/backend/so_fake.py
+++ project/backend/so_fake.py
@@ -127,12 +127,6 @@ class TrueModel:
             bbox_img = cv2.cvtColor(bbox_img, cv2.COLOR_RGB2BGR)
             x1, y1, x2, y2 = map(int, self.bbox)  # Make sure bbox are integers
 
-            # Draw rectangle
-            x1 = int(x1 * self.x_factor)
-            y1 = int(y1 * self.y_factor)
-            x2 = int(x2 * self.x_factor)
-            y2 = int(y2 * self.y_factor)
-            
             cv2.rectangle(bbox_img, (x1, y1), (x2, y2), (0, 255, 0), thickness=4)  # Green bbox
             # Optionally, add label
             cv2.putText(
